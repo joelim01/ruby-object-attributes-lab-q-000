254a8e9633438c9eb78a8498dd082b21ee0cf40d
Joseph Lim <joseph.d.lim@gmail.com>
2016-05-13 17:12:00 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+
+    def name
+      @name
+    end
+    def name=(name)
+      @name = name
+    end
+    def breed
+      @breed
+    end
+    def breed=(breed)
+      @breed = breed
+    end
+
+  def bark()
+    puts "Woof!"
+  end
+  def sit()
+    puts "The Dog is sitting"
+  end
+end
